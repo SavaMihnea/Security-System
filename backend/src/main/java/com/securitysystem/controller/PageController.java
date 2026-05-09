4986c7f82ea349d2c446b2aa382cef6318fc5d34
Sava Mihnea <sava.mi.mihnea@student.utcluj.ro>
2026-05-09 12:19:55 +0300

    About page

--- backend/src/main/java/com/securitysystem/controller/PageController.java
+++ backend/src/main/java/com/securitysystem/controller/PageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-    @GetMapping("/login")
+    @GetMapping({"/", "/login"})
     public String login() { return "forward:/login.html"; }
 
     @GetMapping("/dashboard")
